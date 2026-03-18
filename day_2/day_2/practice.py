92f5db9de381f349c94826ac4fb3172da7e56a93
tiffany w. <91567004+tiffanynwilliams@users.noreply.github.com>
2026-03-18 12:25:59 -0400

    Add current schoolwork

---
+++ day_2/day_2/practice.py
@@ -0,0 +1,28 @@
+# Python Full Course for Beginners by Mosh [31:57]
+students_count = 1000
+
+# float = number with decimals
+rating = 4.99
+
+# boolean = True or False; remember the case
+is_published = False
+
+# string = sequence of characters, wrapped in double quotes
+course = "Python Programming"
+
+# Triple quotes """ can be used for multi-line strings
+message = """
+Hi Johnny Appleseed,
+
+What do you think of the empire apple?
+
+Regards,
+Tiffany Williams"""
+
+# the len() function returns no. of characters in a string (includes spaces)
+# calling a function = function name followed by parentheses
+# arguments = values passed to a function (in this case, the string)
+print(len(course))
+
+# access specific characters in a string with index [] (0 will return first character, 1 will return second character, and so on)
+print(course[0])
